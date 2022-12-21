7260badc7f67c34dba608d6d251f72ecf6301f74
p-omahony <paul.omahony@outlook.fr>
2022-12-21 13:28:07 +0100

    add dataclasses + geodistance filter

--- backend/gas.py
+++ backend/gas.py
@@ -1,20 +1,69 @@
 import requests
 import pandas as pd
+from dataclasses import dataclass
+from typing import List
 
 class GasDriver:
     def __init__(self) -> None:
         self.url = 'https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-carburants-fichier-instantane-test-ods-copie'
 
-    def query(self, filters=None):
-        query = self.url + '&q=&facet=id&facet=adresse&facet=ville&facet=prix_maj&facet=prix_nom&facet=cp&facet=com_arm_name&facet=epci_name&facet=dep_name&facet=reg_name&facet=services_service&facet=horaires_automate_24_24'
+    def get_data(self, facets=None, filters=None, distance_from_point=(None, None, None)):
+        query = self.url + '&q='
+        if facets is not None:
+            for facet in facets:
+                query += '&facet=%s'%facet
         if filters is not None:
             for f in filters :
                 query += '&refine.%s'%f
+        if all(i is not None for i in distance_from_point):
+            query += '&geofilter.distance=' + distance_from_point[0] + '%2C' + distance_from_point[1] +  '%2C' + distance_from_point[2]
         r = requests.get(query)
         res = r.json()
         return res['records']
 
+@dataclass
+class Point:
+    latitude: float
+    longitude: float
+
+
+@dataclass
+class Gas:
+    name: str
+    prix: float
+
+@dataclass
+class GasStation:
+    address: str
+    cp: str
+    coords: Point
+    fuels: List[Gas]
+
+    def __eq__(self, __o: object) -> bool:
+        return self.coords == __o.coords
+
+
+
+
 if __name__ == '__main__' :
     driver = GasDriver()
-    data = driver.query(filters=['ville=Paris', 'cp=75019']) #renvoie la dernière màj du prix de chaque type de carburant pour toutes les stations du 19ème à Paris
-    print(data[0])
+    # distance_from_point=("48.8520930694", "2.34738897685", "1000")
+    data = driver.get_data(facets=['id', 'geom', 'prix_nom'], filters=['ville=Paris'], distance_from_point=("48.8520930694", "2.34738897685", "10000")) #renvoie la dernière màj du prix de chaque type de carburant pour toutes les stations du 19ème à Paris
+    gas_stations = []
+    for r in data:
+        fields = r['fields']
+        try:
+            prix_nom = fields['prix_nom']
+            prix_valeur = fields['prix_valeur']
+        except:
+            prix_nom = "Nous disposons pas d'information"
+            prix_valeur = None
+        gas_station = GasStation(fields['adresse'], fields['cp'], Point(float(fields['geom'][0]), float(fields['geom'][1])), fuels=[Gas(prix_nom, prix_valeur)])
+        if  gas_station not in gas_stations:
+            gas_stations.append(gas_station)
+        else : 
+            id = [i for i,x in enumerate(gas_stations) if x == gas_station][0]
+            gas_stations[id].fuels.append(Gas(fields['prix_nom'], fields['prix_valeur']))
+
+
+    print(gas_stations)
